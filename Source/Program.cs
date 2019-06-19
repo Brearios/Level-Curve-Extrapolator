95212b7fd5a875a0dc3624350a620f4038e5929a
Aaron Olson <presin16@gmail.com>
2019-06-19 11:43:09 -0500

    Renamed, user-selectable loop count, int > long.

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,21 +1,28 @@
-﻿using System;
+using System;
 
-namespace LevelPaceMultiplicator
+namespace LevelCurveExtrapolator
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Level Pace Multiplicator!");
-            Console.WriteLine("Use this to choose a \"pace\" for a given stat while leveling, and it will give you values for your stat up to level 100.");
+            Console.WriteLine("Welcome to the Level Curve Extrapolator!");
+            Console.WriteLine("Use this to choose a \"curve\" for a given stat up to a chosen level, and it will give you each level's values (up to 9.2~ quintillion.)");
 
             Console.WriteLine("To begin, enter the value of your stat at level 1.");
             double levelOneStat = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Now how much higher should this be at level 10? Enter 1.5x, 2x, 2.5x, 5x, 10x or 20x.");
+            Console.WriteLine("How much higher should this be at level 10? Enter 1.5x, 2x, 2.5x, 5x, 10x or 20x.");
             string levelTenRatio = Console.ReadLine();
-            int levelUpLevel = 2;
+
+            int levelUpLevel = 2; // Starting value for the loops
+
+            Console.WriteLine("How many levels do you need data for?");
+            int loopCount = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Here are your values based on a rate that increases " + levelTenRatio + "at level 10, going to level " + loopCount);
+            // This is unneeded because levelUpLevel is set to two. int loopCount = loopCount -=;
 
             Console.WriteLine("Level 1: " + levelOneStat);
 
@@ -25,9 +32,9 @@ namespace LevelPaceMultiplicator
             {
                 double levelUpMultiplier = 1.0461;
                 double levelUpStat = levelOneStat * levelUpMultiplier;
-                while (levelUpLevel < 101)
+                while (levelUpLevel <= loopCount)
                 {
-                    Console.WriteLine("Level " + levelUpLevel + ": " + (int)levelUpStat);
+                    Console.WriteLine("Level " + levelUpLevel + ": " + (long)levelUpStat);
                     levelUpLevel++;
                     levelUpStat = levelUpStat * levelUpMultiplier;
                 }
@@ -36,9 +43,9 @@ namespace LevelPaceMultiplicator
             {
                 double levelUpMultiplier = 1.0805;
                 double levelUpStat = levelOneStat * levelUpMultiplier;
-                while (levelUpLevel < 101)
+                while (levelUpLevel <= loopCount)
                 {
-                    Console.WriteLine("Level " + levelUpLevel + ": " + (int)levelUpStat);
+                    Console.WriteLine("Level " + levelUpLevel + ": " + (long)levelUpStat);
                     levelUpLevel++;
                     levelUpStat = levelUpStat * levelUpMultiplier;
                 }
@@ -47,9 +54,9 @@ namespace LevelPaceMultiplicator
             {
                 double levelUpMultiplier = 1.1072;
                 double levelUpStat = levelOneStat * levelUpMultiplier;
-                while (levelUpLevel < 101)
+                while (levelUpLevel <= loopCount)
                 {
-                    Console.WriteLine("Level " + levelUpLevel + ": " + (int)levelUpStat);
+                    Console.WriteLine("Level " + levelUpLevel + ": " + (long)levelUpStat);
                     levelUpLevel++;
                     levelUpStat = levelUpStat * levelUpMultiplier;
                 }
@@ -58,9 +65,9 @@ namespace LevelPaceMultiplicator
             {
                 double levelUpMultiplier = 1.1959;
                 double levelUpStat = levelOneStat * levelUpMultiplier;
-                while (levelUpLevel < 101)
+                while (levelUpLevel <= loopCount)
                 {
-                    Console.WriteLine("Level " + levelUpLevel + ": " + (int)levelUpStat);
+                    Console.WriteLine("Level " + levelUpLevel + ": " + (long)levelUpStat);
                     levelUpLevel++;
                     levelUpStat = levelUpStat * levelUpMultiplier;
                 }
@@ -69,9 +76,9 @@ namespace LevelPaceMultiplicator
             {
                 double levelUpMultiplier = 1.2916;
                 double levelUpStat = levelOneStat * levelUpMultiplier;
-                while (levelUpLevel < 101)
+                while (levelUpLevel <= loopCount)
                 {
-                    Console.WriteLine("Level " + levelUpLevel + ": " + (int)levelUpStat);
+                    Console.WriteLine("Level " + levelUpLevel + ": " + (long)levelUpStat);
                     levelUpLevel++;
                     levelUpStat = levelUpStat * levelUpMultiplier;
                 }
@@ -80,9 +87,9 @@ namespace LevelPaceMultiplicator
             {
                 double levelUpMultiplier = 1.395;
                 double levelUpStat = levelOneStat * levelUpMultiplier;
-                while (levelUpLevel < 101)
+                while (levelUpLevel <= loopCount)
                 {
-                    Console.WriteLine("Level " + levelUpLevel + ": " + (int)levelUpStat);
+                    Console.WriteLine("Level " + levelUpLevel + ": " + (long)levelUpStat);
                     levelUpLevel++;
                     levelUpStat = levelUpStat * levelUpMultiplier;
                 }
